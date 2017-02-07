3aba686787c7c71044252cad4132d9e0755c60f2
Stefan Kral <me@stnk.de>
2017-02-07 10:48:36 +0100

    Fix

--- src/tides.php
+++ src/tides.php
@@ -15,17 +15,16 @@ echo '
 <table cellpadding="'.$table_cellpadding.'" cellspacing="0" width="'.$table_width.'" summary="Vorhersage">
 
 <tr height="1">
-<th width="15%"></th>
-<th width="17%"></th>
-<th width="17%"></th>
-<th width="17%"></th>
-<th width="17%"></th>
-<th width="17%"></th>
+<th width="20%"></th>
+<th width="20%"></th>
+<th width="20%"></th>
+<th width="20%"></th>
+<th width="20%"></th>
 </tr>
 
 ';
 
-for ($i=0; $i<=30; $i++) {
+for ($i=0; $i<=6; $i++) {
 	$today = '';
 	switch ($i) {
 	    case 0:
@@ -43,7 +42,7 @@ for ($i=0; $i<=30; $i++) {
 	}
 	echo '
 <tr>
-<td colspan="6"><br /><big><b>'.$today.strftime('%A, der %d.%m.%Y',intval($sun_data[$i]['date'])).'</b></big><br />&nbsp;</td>
+<td colspan="5"><br /><big><b>'.$today.strftime('%A, der %d.%m.%Y',intval($sun_data[$i]['date'])).'</b></big><br />&nbsp;</td>
 </tr>
 ';
 
@@ -55,56 +54,38 @@ if ($bsh_tides=='yes' && $i<=6) {
 <td align="center"><i class="wi '.weather_icon($tide_text[$tide_date[$i]][1][2]).'"></i><br />'.$tide_text[$tide_date[$i]][1][1].'</td>
 <td align="center"><i class="wi '.weather_icon($tide_text[$tide_date[$i]][2][2]).'"></i><br />'.$tide_text[$tide_date[$i]][2][1].'</td>
 <td align="center"><i class="wi '.weather_icon($tide_text[$tide_date[$i]][3][2]).'"></i><br />'.$tide_text[$tide_date[$i]][3][1].'</td>
-<td align="center">&nbsp;</td>
 </tr>
 ';
 }
 
 echo '
 <tr>
-<td>Sonne</td>
-<td align="center"><span class="small">
-AD: '.strftime('%H:%M',intval($sun_data[$i]['astronomical_twilight_begin'])).'<br />
-ND: '.strftime('%H:%M',intval($sun_data[$i]['nautical_twilight_begin'])).'<br />
-BD: '.strftime('%H:%M',intval($sun_data[$i]['civil_twilight_begin'])).'
-</span></td>
+<td>Sonne / Mond</td>
 <td align="center"><i class="wi wi-sunrise"></i><br />'.strftime('%H:%M',intval($sun_data[$i]['sunrise'])).'</td>
-<td align="center"><span class="small">Zenith:</span><br />'.strftime('%H:%M',intval($sun_data[$i]['transit'])).'</td>
 <td align="center"><i class="wi wi-sunset"></i><br />'.strftime('%H:%M',intval($sun_data[$i]['sunset'])).'</td>
-<td align="center"><span class="small">
-BD: '.strftime('%H:%M',intval($sun_data[$i]['civil_twilight_end'])).'<br />
-ND: '.strftime('%H:%M',intval($sun_data[$i]['nautical_twilight_end'])).'<br />
-AD: '.strftime('%H:%M',intval($sun_data[$i]['astronomical_twilight_end'])).'
-</span></td>
-</tr>
-
-<tr>
-<td>Mond</td>
 ';
 if ($moon_data[$i]['moonrise'] > $moon_data[$i]['moonset']) {
 	echo '<td align="center"><i class="wi wi-moonset"></i><br />'.strftime('%H:%M',intval($moon_data[$i]['moonset'])).'</td>
-<td align="center"><i class="wi wi-moonrise"></i><br />'.strftime('%H:%M',intval($moon_data[$i]['moonrise'])).'</td>
-';
+<td align="center"><i class="wi wi-moonrise"></i><br />'.strftime('%H:%M',intval($moon_data[$i]['moonrise'])).'</td>';
 } else {
 	echo '<td align="center"><i class="wi wi-moonrise"></i><br />'.strftime('%H:%M',intval($moon_data[$i]['moonrise'])).'</td>
-<td align="center"><i class="wi wi-moonset"></i><br />'.strftime('%H:%M',intval($moon_data[$i]['moonset'])).'</td>
-';
+<td align="center"><i class="wi wi-moonset"></i><br />'.strftime('%H:%M',intval($moon_data[$i]['moonset'])).'</td>';
 }
-echo '<td align="center"><i class="wi '.moonphase_icon($moon_data[$i]['phase']).'"></i><br /><span class="small">'.$moon_data[$i]['phase_name'].'<br />('.$moon_data[$i]['illuminated'].' sichtbar)</span></td>
-<td colspan="2" align="center"><span class="small">Mondalter: '.$moon_data[$i]['age'].'<br />Mondphase: '.$moon_data[$i]['phase'].'&nbsp;%<br />Entfernung: '.$moon_data[$i]['distance'].'</span></td>
+
+echo '
 </tr>
 
 <tr>
-<td colspan="6"><hr></td>
+<td colspan="5"><hr></td>
 </tr>
 ';
 }
 
 echo '
 <tr>
-<td colspan="6"><div class="small" align="right">';
+<td colspan="5"><div class="small" align="right">';
 if ($bsh_tides=='yes') {
-	echo 'Gezeiten: die Veröffentlichung erfolgt mit Genehmigung des <a rel="nofollow" target="_blank" title="Bundesamt für Seeschifffahrt und Hydrographie" href="http://www.bsh.de/">BSH</a> | ';
+	echo 'Gezeiten: die Veröffentlichung erfolgt mit Genehmigung des <a rel="nofollow" target="_blank" title="Bundesamt für Seeschifffahrt und Hydrographie" href="http://www.bsh.de/">BSH</a> (7 Tage) | ';
 }
 echo 'Sonne/Mond: berechnet</div></td>
 </tr>
